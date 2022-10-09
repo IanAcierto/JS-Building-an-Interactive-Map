83c0eaec37a19e7a28f0ffd81964040c7396be0b
Ian Acierto <acierto_vincent@yahoo.com>
2022-10-08 20:33:11 -0700

    set up functions and html

---
+++ script.js
@@ -0,0 +1,23 @@
+const myMap = {
+
+}
+
+async function getCoords(){
+
+}
+
+async function getFourSquare(){
+
+}
+
+function processBusinesses(data){
+
+}
+
+window.onload = async()=>{
+
+}
+
+document.getElementById('submit').addEventListener('click', async (event)=>{
+  
+})
